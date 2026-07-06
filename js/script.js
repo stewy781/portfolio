ecc6b16aa329fc683112a18cd81e5dda4af3dd5b
Heather Stewart <stewy781@hotmail.com>
2026-07-06 09:24:05 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function runTypingEffect() {
+    const text = 'I am Heather Larson!';
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 100;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, typingDelay) {
+
+    for (let i = 0; i < text.length; i++) {
+        setTimeout(() => {
+            typingElement.textContent += text.charAt(i);
+
+         }, typingDelay * i );
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
