60e2ffcd66017f5f61d319c4f6eb82f8ba72778d
Heather Stewart <stewy781@hotmail.com>
2026-07-13 12:09:00 -0500

    Added updated files

--- js/script.js
+++ js/script.js
@@ -16,4 +16,32 @@ function typeText(text, typingElement, typingDelay) {
     }
 }
 
+
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+    const toTopBtn = document.querySelector('#to-top');
+
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 1) {
+                navbar.classList.add('bg-secondary');
+                navbar.classList.add('navbar-sticky');
+                toTopBtn.classList.add('show');
+            }
+            else {
+                navbar.classList.remove('bg-secondary');
+                navbar.classList.remove('navbar-sticky');
+                toTopBtn.classList.remove('show');
+           }
+        })
+}
+
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//Event Listeners
 document.addEventListener('DOMContentLoaded', runTypingEffect);
+document.addEventListener('DOMContentLoaded', userScroll);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
